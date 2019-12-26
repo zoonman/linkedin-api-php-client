e991a3587d74f3275ddec0451b8aa187330f47f3
Philipp <zoonman@gmail.com>
2019-12-26 00:22:38 -0500

    Update Scope.php

--- src/Scope.php
+++ src/Scope.php
@@ -27,6 +27,13 @@ class Scope extends AbstractEnum
      * Allows to read basic information about profile, such as name
      */
     const READ_BASIC_PROFILE = 'r_basicprofile';
+    /**
+     * Request a minimum information about the user
+     * Use this scope when implementing "Sign In with LI"
+     */
+    const READ_LITE_PROFILE = 'r_liteprofile';
+    
+    const READ_FULL_PROFILE = 'r_fullprofile';
 
     /**
      * Enables access to email address field
@@ -46,4 +53,9 @@ class Scope extends AbstractEnum
      * Manage and delete your data including your profile, posts, invitations, and messages
      */
     const COMPLIANCE = 'w_compliance';
+    
+    const SHARE_AS_ORGANIZATION = 'w_organization_social';
+    const READ_ORGANIZATION_SHARES = 'r_organization_social';
+    const SHARE_AS_USER = 'w_member_social';
+    const ADS_MANAGEMENT = 'rw_ads';
 }
