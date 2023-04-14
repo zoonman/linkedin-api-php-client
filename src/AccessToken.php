ffe18dc52481e59d788e0a675f1f4182c9a45bd0
Esens Design <103586317+esensgit@users.noreply.github.com>
2023-04-14 12:58:40 +0200

    Removed validation of refresh data.
    No clue if this works.

--- src/AccessToken.php
+++ src/AccessToken.php
@@ -225,17 +225,17 @@ class AccessToken implements \JsonSerializable
     private static function validateRefreshToken($responseArray)
     {
         if (!isset($responseArray['refresh_token'])) {
-            throw new \InvalidArgumentException(
-                'Refresh token is not available'
-            );
+           // throw new \InvalidArgumentException(
+           //    'Refresh token is not available'
+           // );
         }
     }
     private static function validateRefreshTokenExpiresIn($responseArray)
     {
         if (!isset($responseArray['refresh_token_expires_in'])) {
-            throw new \InvalidArgumentException(
-                'Refresh token expiration date is not specified'
-            );
+            //throw new \InvalidArgumentException(
+            //    'Refresh token expiration date is not specified'
+            //);
         }
     }
     /**
